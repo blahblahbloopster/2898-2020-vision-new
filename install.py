be2d890f0860cd4a06e5e8756abc4c4ccea01a75
Max Leibowitz <403089@bsd48.org>
2020-03-03 17:00:11 -0800

    Fixed installation script

--- install.py
+++ install.py
@@ -16,31 +16,11 @@ os.system("apt autoremove")
 os.system("apt -yqq install python3-pip python3-opencv git")
 os.system("cd /home/pi/")
 os.system("git clone https://github.com/blahblahbloopster/2898-2020-vision-new.git")
-os.system("cd /home/pi/2898-2020-vision-new/")
+
 try:
-    os.system("pip3 install -r requirements.txt")
+    os.system("pip3 install -r /home/pi/2898-2020-vision-new/requirements.txt")
 except:
     pass
-with open("/lib/systemd/system/vision.service", "wt") as f:
-    f.write("""[Unit]
-Description=2898's 2020 FRC vision code
-
-[Service]
-Type=simple
-ExecStart=/bin/python3 /home/pi/2898-2020-vision-new/HexFinder.py
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target""")
-
-
-with open("/lib/systemd/system/load-code.service") as f:
-    f.write("""[Unit]
-Description=Loads 2898's 2020 FRC vision code
-
-[Service]
-Type=oneshot
-ExecStart=/bin/python3 /home/pi/2898-2020-vision-new/update_code.py
-
-[Install]
-WantedBy=multi-user.target""")
+os.system('echo "[Unit]\nDescription=2898\'s 2020 FRC vision code\n\n[Service]\n'
+          'Type=simple\nExecStart=/bin/python3 /home/pi/2898-2020-vision-new/HexFinder.py\n'
+          'Restart=on-failure\n\n[Install]\nWantedBy=multi-user.target" >> /lib/systemd/system/vision.service')
